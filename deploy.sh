924b33878304efa8c4523c32350457e3b433a164
fvhome-vr <vryazaev@gmail.com>
2026-04-05 22:34:15 +0300

    deploy script. fix some bugs

--- deploy.sh
+++ deploy.sh
@@ -4,30 +4,19 @@ source .env
 
 if [ ! -h "${CONF_DIR}/nginx/certs/${DOMAIN}.crt" ]; then
     echo "Start setup.."
-    mkdir -p "${DATA_DIR}/prometheus"
-    mkdir -p "${MEDIA_LIBARY}/{music,photo,video}"
-    mkdir -p "${MEDIA_LIBARY}/transmission/{complete,incomplete}"
+    mkdir -p ${DATA_DIR}/{prometheus,nextcloud}
+    mkdir -p ${MEDIA_LIBARY}/{music,photo,video}
+    mkdir -p ${MEDIA_LIBARY}/transmission/{complete,incomplete}
     chown "${WWW_USER_UID}":"${WWW_USER_GID}" -R "${DATA_DIR}" "${MEDIA_LIBARY}"
     chmod g+s -R "${DATA_DIR}" "${MEDIA_LIBARY}"
-    
-    docker compose up -d --profile setup --abort-on-container-exit
-    if [ $? -ne 0 ]; then
-        echo "Setup error. Stop"
-        exit 1
-    fi
-    docker compose rm -f
+    docker compose --profile setup up --abort-on-container-exit
 fi
 
-docker compose up -d --profile main
-if [ $? -ne 0 ]; then
-    echo "Deploy error. Stop"
-    exit 1
+# exit
+
+echo "Deploy main services.."
+if [ -h "${CONF_DIR}/nginx/certs/${DOMAIN}.crt" ]; then
+  docker compose --profile main up -d
 fi
 
-if [ "${DEPLOY_MONITORING}" = "true" ]; then
-    docker compose up -d --profile monitoring
-    if [ $? -ne 0 ]; then
-        echo "Deploy monitoring. Stop"
-        exit 1
-    fi
-fi
+exit
