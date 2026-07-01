8f5b4ac3f4d595c3501a5a3f2a6a8afe5151b03d
root <vryazaev@gmail.com>
2026-07-02 01:03:06 +0300

    Fix CTRL-C and remove setup services

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,39 @@ source .env
 
 if [ ! -h "${CONF_DIR}/nginx/certs/${DOMAIN}.crt" ]; then
     echo "Start setup.."
+    DOCKER80=$(docker ps -f status=running -f publish=80 -q)
+    [ -n "${DOCKER80}" ] && docker stop "${DOCKER80}" && docker rm -f nginx
     mkdir -p ${DATA_DIR}/{prometheus,nextcloud}
     mkdir -p ${MEDIA_LIBARY}/{music,photo,video}
     mkdir -p ${MEDIA_LIBARY}/transmission/{complete,incomplete}
     chown "${WWW_USER_UID}":"${WWW_USER_GID}" -R "${DATA_DIR}" "${MEDIA_LIBARY}"
     chmod g+s -R "${DATA_DIR}" "${MEDIA_LIBARY}"
-    docker compose --profile setup up --abort-on-container-exit
-fi
 
-# exit
+    cleanup_setup() {
+        echo -e "\nSetup interrupted by user. Stopping and removing setup containers..."
+        kill $SETUP_PID 2>/dev/null
+        docker compose --profile setup down
+    }
+
+    trap 'cleanup_setup' INT
+
+    (docker compose --profile setup up --abort-on-container-exit 2>&1 | \
+      grep --line-buffered  "letsencrypt_setup") &
+    SETUP_PID=$!
+
+    while kill -0 $SETUP_PID 2>/dev/null; do
+        echo 'If the certificate is received, then use CTRL+C to go to the main deploy';
+        sleep 5
+    done
+
+    trap - INT
+fi
 
 echo "Deploy main services.."
 if [ -h "${CONF_DIR}/nginx/certs/${DOMAIN}.crt" ]; then
   docker compose --profile main up -d
+else
+  echo "Error: Certificate not found. Stop"
 fi
 
 exit
